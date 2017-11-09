8fd09cfce93083da3c4522640326cec90d409317
danielhuang <danielhuang030@gmail.com>
2017-11-09 01:24:30 -0800

    feat: add Authorization Bearer token

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -15,6 +16,18 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 )
+
+// User
+type User struct {
+	ID            uint `gorm:"primary_key"`
+	Name          string
+	Email         string
+	Password      string
+	ApiToken      string
+	RememberToken string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
 
 // Account
 type Account struct {
@@ -35,19 +48,7 @@ type Setting struct {
 	UpdatedAt time.Time
 }
 
-// // Response
-// type Response struct {
-// 	statusCode int
-// 	message    string
-// }
-//
-// func getResponse() Response {
-// 	response := Response{}
-// 	response.statusCode = http.StatusBadRequest
-// 	response.message = "Error"
-// 	return response
-// }
-
+// init
 func init() {
 	err := godotenv.Load()
 	if err != nil {
@@ -55,60 +56,101 @@ func init() {
 	}
 }
 
-func main() {
-	router := gin.Default()
-	router.POST("/api/v1/accounts", func(c *gin.Context) {
-		// connect DB
-		db, err := gorm.Open(os.Getenv("DB_CONNECTION"), fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE")))
-		if err != nil {
-			statusCode := http.StatusBadRequest
-			c.JSON(statusCode, gin.H{
-				"statusCode": statusCode,
-				"message":    err.Error(),
-				"data":       "",
-			})
+//JWTAuthMiddleware middleware
+func JWTAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if !validateToken(c) {
+			c.Abort()
 			return
 		}
+		c.Next()
+		return
+	}
+}
 
-		// add account
-		newAccount, err := createAccount(db, c.PostForm("account"))
-		if err != nil {
-			statusCode := http.StatusBadRequest
-			c.JSON(statusCode, gin.H{
-				"statusCode": statusCode,
-				"message":    err.Error(),
-				"data":       "",
-			})
-			return
-		}
-		defer db.Close()
+// validate token
+func validateToken(c *gin.Context) bool {
+	token := c.Request.Header.Get("Authorization")
+	token = strings.TrimPrefix(token, "Bearer ")
+	if token == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"statusCode": http.StatusUnauthorized,
+			"message":    "Unauthorized Error",
+			"data":       "",
+		})
+		return false
+	}
 
-		// var account Account
-		// db.Find(&account, "id = ?", 3)
-		// fmt.Printf("%+v", whatever)
-		// account.Account = "changeByGo"
-		// db.Save(&account)
-
-		statusCode := http.StatusOK
+	// connect DB
+	db, err := gorm.Open(os.Getenv("DB_CONNECTION"), fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+	if err != nil {
+		statusCode := http.StatusBadRequest
 		c.JSON(statusCode, gin.H{
 			"statusCode": statusCode,
-			"message":    "Success",
-			"data":       newAccount,
+			"message":    err.Error(),
+			"data":       "",
 		})
-	})
+		return false
+	}
+
+	// get user token
+	var user User
+	db.Where("api_token = ?", token).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"statusCode": http.StatusUnauthorized,
+			"message":    "Unauthorized Error",
+			"data":       "",
+		})
+		return false
+	}
+	return true
+}
+
+func main() {
+	router := gin.Default()
+	api := router.Group("/api/v1")
+	api.Use(JWTAuthMiddleware())
+	{
+		api.POST("/accounts", func(c *gin.Context) {
+			// connect DB
+			db, err := gorm.Open(os.Getenv("DB_CONNECTION"), fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+			if err != nil {
+				statusCode := http.StatusBadRequest
+				c.JSON(statusCode, gin.H{
+					"statusCode": statusCode,
+					"message":    err.Error(),
+					"data":       "",
+				})
+				return
+			}
+
+			// add account
+			newAccount, err := createAccount(db, c.PostForm("account"))
+			if err != nil {
+				statusCode := http.StatusBadRequest
+				c.JSON(statusCode, gin.H{
+					"statusCode": statusCode,
+					"message":    err.Error(),
+					"data":       "",
+				})
+				return
+			}
+			defer db.Close()
+
+			// Success
+			statusCode := http.StatusOK
+			c.JSON(statusCode, gin.H{
+				"statusCode": statusCode,
+				"message":    "Success",
+				"data":       newAccount,
+			})
+		})
+	}
 	router.Run(":4000")
 }
 
-// func checkErr(err error, response Response) Response {
-// 	if err != nil {
-// 		response.message = err.Error()
-// 		// fmt.Printf("%v", err)
-// 		// panic(err)
-// 	}
-// 	return response
-// }
-// func render
-
+// get random password
 func getRandomPassword(strlen int) string {
 	const POOL = "abcdefghijkmnpqrstuwxyz23456789"
 	password := make([]byte, strlen)
@@ -118,23 +160,7 @@ func getRandomPassword(strlen int) string {
 	return string(password)
 }
 
-// func getNextIp(db *gorm.DB, skey string) string {
-// 	if skey == "" {
-// 		skey = "private_ip_member"
-// 	}
-// 	var setting Setting
-// 	db.Find(&setting, "skey = ?", skey)
-// 	ip := net.ParseIP(setting.Svalue)
-// 	ip = ip.To4()
-// 	ip[3]++
-//
-// 	newIp := ip.String()
-// 	setting.Svalue = newIp
-// 	db.Save(&setting)
-//
-// 	return newIp
-// }
-
+// create account
 func createAccount(db *gorm.DB, account string) (Account, error) {
 	tx := db.Begin()
 
@@ -169,33 +195,3 @@ func createAccount(db *gorm.DB, account string) (Account, error) {
 
 	return newAccount, nil
 }
-
-/*
-package main
-
-import (
-  "database/sql"
-  "fmt"
-
-  _ "github.com/go-sql-driver/mysql"
-)
-
-func main() {
-  db, err := sql.Open("mysql", "root:@/vpn?charset=utf8mb4")
-  checkErr(err)
-  rows, err := db.Query("SELECT id, account FROM accounts WHERE id = 1")
-  checkErr(err)
-  fmt.Println(rows)
-
-  for rows.Next() {
-    var uid int
-    var account string
-
-    err = rows.Scan(&uid, &account)
-    checkErr(err)
-
-    fmt.Println(uid)
-    fmt.Println(account)
-  }
-}
-*/
