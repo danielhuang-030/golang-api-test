2af7598fefa210a3e5c95e16980e5db33553208c
danielhuang <danielhuang030@gmail.com>
2017-11-15 00:33:09 -0800

    refactor: add db init in Model.go

--- models/User.go
+++ models/User.go
@@ -1,16 +1,30 @@
 package models
 
 import (
-  "time"
+	"time"
 )
 
 type User struct {
-  ID            uint `gorm:"primary_key"`
-  Name          string
-  Email         string
-  Password      string
-  ApiToken      string
-  RememberToken string
-  CreatedAt     time.Time
-  UpdatedAt     time.Time
+	ID            uint `gorm:"primary_key"`
+	Name          string
+	Email         string
+	Password      string
+	ApiToken      string
+	RememberToken string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+// is auth by token
+func IsAuthByToken(token string) bool {
+	if token == "" {
+		return false
+	}
+
+	var user User
+	GetDB().Find(&user, "api_token = ?", token)
+	if user.ID == 0 {
+		return false
+	}
+	return true
 }
